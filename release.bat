e5aa8ae1392c3e6db1eca8286b25922688292e0f
Xomega.Net <misc@xomega.net>
2016-02-19 00:08:55 -0500

    Added license, improved release script

--- release.bat
+++ release.bat
@@ -1,8 +1,30 @@
 SET VER=%1
-if exist "pkg\content\" rd /q /s "pkg\content"
+SET NUGET_PATH=.nuget\NuGet.exe
+SET NUGET_VERSION=latest
+SET CACHED_NUGET=%LocalAppData%\NuGet\nuget.%NUGET_VERSION%.exe
+
+IF NOT EXIST .nuget md .nuget
+IF NOT EXIST %NUGET_PATH% (
+  IF NOT EXIST %CACHED_NUGET% (
+    echo Downloading latest version of NuGet.exe...
+    IF NOT EXIST %LocalAppData%\NuGet ( 
+      md %LocalAppData%\NuGet
+    )
+    @powershell -NoProfile -ExecutionPolicy unrestricted -Command "$ProgressPreference = 'SilentlyContinue'; Invoke-WebRequest 'https://dist.nuget.org/win-x86-commandline/%NUGET_VERSION%/nuget.exe' -OutFile '%CACHED_NUGET%'"
+  )
+
+  copy %CACHED_NUGET% %NUGET_PATH% > nul
+)
+
+IF EXIST "pkg\content\" rd /q /s "pkg\content"
 xcopy "bin\xomega.d.ts" "pkg\content\Scripts\typings\xomegajs\" >nul
-if exist "pkg\XomegaJS.%VER%" rd /q /s "pkg\XomegaJS.%VER%"
-mkdir "pkg\XomegaJS.%VER%"
+IF EXIST "pkg\XomegaJS.%VER%" rd /q /s "pkg\XomegaJS.%VER%"
+md "pkg\XomegaJS.%VER%"
 copy "bin\xomega.js" "pkg\content\Scripts\xomega-%VER%.js" >nul
-call replace.bat {version} %VER% Package.nuspec > pkg\Package.nuspec
-packages\NuGet.CommandLine.3.3.0\tools\NuGet.exe pack "pkg\Package.nuspec" -OutputDirectory "pkg\XomegaJS.%VER%"
+
+@powershell (Get-Content -raw Package.nuspec) -replace '{version}', '%VER%' -replace '.*^<dependencies.*', ^
+((Get-Content -raw packages.config) -replace '^.\?.*\n', '' ^
+-replace 'packages', 'dependencies' -replace 'package', 'dependency' -replace ' targetFramework=\".*?\"', '' ^
+-replace '(^<)', '________$1' -replace ' +_', '_____' -replace '_', ' ') > pkg\Package.nuspec
+
+%NUGET_PATH% pack "pkg\Package.nuspec" -OutputDirectory "pkg\XomegaJS.%VER%"
