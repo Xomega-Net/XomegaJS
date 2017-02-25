ad101fec26b2c74a06e7251e10d1f85b63fa58cd
Xomega.Net <misc@xomega.net>
2017-02-25 18:18:12 -0500

    extended input text binding for other html5 input types

--- Binding/InputTextBinding.ts
+++ Binding/InputTextBinding.ts
@@ -5,9 +5,11 @@ module xomega {
     // property binding for input text controls
     export class InputTextBinding extends PropertyBinding {
 
+        public static inputTypes: Array<string> = ['text', 'password', 'email', 'tel', 'url'];
+
         public appliesTo(element, property): boolean {
             return element.tagName.toLowerCase() == "textarea" ||
-                element.tagName.toLowerCase() == "input" && element.type == "text";
+                element.tagName.toLowerCase() == "input" && InputTextBinding.inputTypes.indexOf(element.type) >= 0;
         }
 
         public init(element, valueAccessor, allBindingsAccessor, viewModel, bindingContext) {
